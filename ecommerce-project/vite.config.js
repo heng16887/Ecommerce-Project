ab34ab5b19706beaf5d28baa6c0fa1fcdba006cd
lim cheaheng <limcheaheng168@gmail.com>
2026-08-03 11:57:30 +0700

    create new react typescript

--- ecommerce-project/vite.config.js
+++ ecommerce-project/vite.config.js
@@ -3,15 +3,21 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react({
+      babel: {
+        plugins: [["babel-plugin-react-compiler", { target: "19" }]],
+      },
+    }),
+  ],
   server: {
     proxy: {
-      '/api': {
-        target: 'http://localhost:3000',
+      "/api": {
+        target: "http://localhost:3000",
       },
-      '/images': {
-        target: 'http://localhost:3000',
-      }
-    }
-  }
-})
+      "/images": {
+        target: "http://localhost:3000",
+      },
+    },
+  },
+});
